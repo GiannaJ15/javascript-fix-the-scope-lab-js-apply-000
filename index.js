7e0a6ef90b6c3fa942691369a8b1e16ccc5621bf
gianna.jolibeth15@gmail.com <gianna.jolibeth15@gmail.com>
2018-08-30 23:26:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction()() //the first set of parenthesis 
+var theFunk = funkyFunction()() //the first set of parenthesis invokes the first funcio
 
